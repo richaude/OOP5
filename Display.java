76b2fff8d55fbc30fe7f7aa72432800b61e9a80f
Darkdragon853 <Lukas.Hempel@diesterweg-gymnasium.de>
2018-06-14 22:30:59 +0200

    RadioButton mit Drin

--- Display.java
+++ Display.java
@@ -1,9 +1,13 @@
 package kochkurven;
 
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.SplitPane;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Display {
@@ -17,18 +21,38 @@ public class Display {
 	
 	
 	public void start() {
+		
+		// Init
+		
 		this.stage.setTitle("Kochkurven - stetige, aber undifferenzierbare Funktionen!");
 		
-		StackPane layoutLeft = new StackPane();
+		VBox layoutLeft = new VBox();
 		StackPane layoutRight = new StackPane();
 		
 		SplitPane layoutControl = new SplitPane(layoutLeft, layoutRight);
 
+		// Elemente
 		Label callMeTestLeft = new Label("THis is a Test! -> I am Left");
 		Label callMeTestRight = new Label("THis is a Test! -> I am Right");
 		
+		ToggleGroup t1 = new ToggleGroup();
+		RadioButton initiator1 = new RadioButton("Initiator 1");
+		RadioButton initiator2 = new RadioButton("Initiator 2");
+		
+		initiator1.setToggleGroup(t1);
+		initiator2.setToggleGroup(t1);
+		// Waehl schon mal einen Aus
+		initiator1.setSelected(true);
+		
+		
+		
+		// Hinzufuegen
+		layoutLeft.getChildren().add(initiator1);
+		layoutLeft.getChildren().add(initiator2);
 		layoutLeft.getChildren().add(callMeTestLeft);
 		layoutRight.getChildren().add(callMeTestRight);
+		
+		// layoutLeft.setAlignment(callMeTestLeft, Pos.BOTTOM_LEFT);
 		
 		Scene scene = new Scene(layoutControl, 500, 500);
 		
