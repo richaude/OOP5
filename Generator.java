f6901a66db02f3ce064ebbbbfbeed95301a323ed
richaude <38363696+richaude@users.noreply.github.com>
2018-06-25 20:11:44 +0200

    Setter für Linie in Generator

---
+++ Generator.java
@@ -0,0 +1,38 @@
+package kochkurven;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Generator {
+
+	private Linie linie;
+	private double minL;
+	private List<Linie> prodLinien;
+	
+	
+	
+	public Generator(Linie linie, double minL) {
+		this.linie = linie; 
+		this.minL = minL;
+	}
+	
+	public Generator() {
+		
+	}
+	
+	public abstract void produziereLinien(boolean innen);
+
+	public List<Linie> getLinien() {
+		prodLinien = new ArrayList<Linie>();
+		return prodLinien;
+	}
+	
+	public Linie getLinie() {
+		return linie;
+	}
+	
+	public void setLinie(double startX, double startY, double endX, double endY) {
+		linie.setLinie(startX, startY, endX, endY);
+	}
+
+}
